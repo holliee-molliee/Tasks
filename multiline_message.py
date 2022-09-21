2be898e8d0edcbc8b6bdfb655104b0a282e1dec1
holliee-molliee <6jakua46@solent.ac.uk>
2022-09-21 12:10:20 +0100

    activity 2.2
    Updated code to display a longer multi-line message.

--- multiline_message.py
+++ multiline_message.py
@@ -1,2 +1,7 @@
 print("System Failure Imminent!\nSystem rebooy has been initiated...")
-
+print()
+print("...rebooying sensory system")
+print("...rebooying output motors")
+print("...rebooying hover engine")
+print()
+print("System online.")
