5606d34695716519a744c80228d66454ebc2352b
holliee-molliee <6jakua46@solent.ac.uk>
2022-09-21 12:14:32 +0100

    activity 3
    Added code to display escape chacters

---
+++ escape_characters.py
@@ -0,0 +1,6 @@
+#Display eescape characters 
+print('\nDisplays a new line')
+print('\tDisplays a tab space')
+print('\\Display a back slash')
+print("\"Displays a double quote")
+print("\'Displays a single quote")
