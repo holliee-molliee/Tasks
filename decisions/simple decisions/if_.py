f319e785d381a6f5bf70789c53e5f029d3b86f4f
holliee-molliee <6jakua46@solent.ac.uk>
2022-10-10 12:21:03 +0100

    Create if_.py

---
+++ decisions/simple decisions/if_.py
@@ -0,0 +1,5 @@
+book = input("What type of book is it?") #user input of book
+if book == "Adventure" or book == "adventure": #checking if user entered the adventure book
+    print("I like adventure books.")
+
+print("Finished reading the book")
