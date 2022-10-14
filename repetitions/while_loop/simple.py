54f067171bb8c57f4a31c16f81df0c74b4574e38
holliee-molliee <6jakua46@solent.ac.uk>
2022-10-14 09:59:49 +0100

    Create simple.py
    Added code to demonstrate the use of a simple while loop.

---
+++ repetitions/while_loop/simple.py
@@ -0,0 +1,5 @@
+cables = int(input("How many cables should I remove?\n"))
+iter = 0 #variable that counts current number of interations
+while iter<cables: #while loop
+    print("Removed cable.") 
+    iter+=1 #update current iteration count
