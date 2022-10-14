f872669267a47a6c00d7178687eea6c680112884
holliee-molliee <6jakua46@solent.ac.uk>
2022-10-14 22:45:28 +0100

    Create simple.py
    Added code to demonstrate the use of a simple for loop.

---
+++ repetitions/for_loop/simple.py
@@ -0,0 +1,9 @@
+mount=int(input("How many mountains should I display?\n"))
+print("Displaying...")
+for i in range(mount):
+    print("          __")
+    print("         /  \_")
+    print("        /^    \ ")
+    print("       /  ^    \_")
+    print('     _/ ^ ^     ^\ ')
+    print("    /  ^     ^    \ ")
