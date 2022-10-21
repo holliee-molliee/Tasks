1fbd3ea3cebdf0f3a0780000f0d5bbcfd92eff7a
holliee-molliee <6jakua46@solent.ac.uk>
2022-10-21 10:27:33 +0100

    Update ascii_character.py
    Added code to demonstrate the use of built-in functions: abs, range, chr.

--- functions/ascii_character.py
+++ functions/ascii_character.py
@@ -1,7 +1,7 @@
 print("Program started!")
 char = abs(int(input("Please enter an ASCII code\n")))
 if char >=32 and char <=126:
-    print("The character represented by the ASCII code {} is {}".format(char,chr(char)))
+    print("The character represented by the ASCII code {} is {}".format(oct(char)[2:],chr(char)))
 else:
     print("Please enter a number in this range <32,126>")
 print("Program ended!")
