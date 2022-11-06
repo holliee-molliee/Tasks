25d712075a5db80a574ccec96df3e9a4db922225
holliee-molliee <6jakua46@solent.ac.uk>
2022-11-06 19:23:55 +0000

    Create export.py
    Added code to export JSON data to a file.

---
+++ data/files/json/export.py
@@ -0,0 +1,21 @@
+import json 
+
+def read(path):
+    print("Reading...")
+    with open(path) as file:
+        data = json.load(file)
+    print("Done!")
+    return data
+
+def save(path, data):
+    print("Exporting...")
+    with open(path, "w") as file:
+        json.dump(data, file,indent=4)
+    print("Done!")
+
+def run():
+    json_data=read("robocity.json")
+    save("exported.json", json_data)
+
+if __name__=="__main__":
+    run()
