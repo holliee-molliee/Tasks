33f3afa1bd44ddd386b073bcc06ecd9a24437c5e
holliee-molliee <6jakua46@solent.ac.uk>
2022-11-09 01:10:47 +0000

    Update titanic.py
    Added code to display the number of each gender.

--- data/titanic/titanic.py
+++ data/titanic/titanic.py
@@ -12,7 +12,7 @@ def load_data(file_path):
     print("Done!")
 
 def display_menu():
-    action = int(input("Please select one of the following options:\n[1] Display the names of all passengers\n[2] Display the number of passengers that survived\n[3] Display the number of passengers per gender\n[4] Display the number of passengers per age group\n\n"))
+    action = int(input("Please select one of the following options:\n[1] Display the names of all passengers\n[2] Display the number of passengers that survived\n[3] Display the number of passengers per gender\n[4] Display the number of passengers per age group\n[5]Display the number of survivors per age group\n\n"))
     return action
 
 def display_passenger_names():
@@ -54,6 +54,34 @@ def display_passengers_per_age_group():
                 elderly+=1
     print(f"children: {children}, adults: {adults}, elderly: {elderly}")
 
+def display_survivors_per_age_group():
+    children,adults,elderly=0,0,0
+    num_c_sur,num_a_sur,num_e_sur=0,0,0
+    for record in records:
+        survival_status=record[1]
+        if int(survival_status)==1:
+            if len(record[5])==0:
+                continue
+            else:
+                age = float(record[5])
+                if age<18:
+                    num_c_sur+=1
+                elif age>=18 and age<65:
+                    num_a_sur+=1
+                else:
+                    num_e_sur+=1
+        if len(record[5])==0:
+            continue
+        else:
+            age = float(record[5])
+            if age<18:
+                children+=1
+            elif age>=18 and age<65:
+                adults+=1
+            else:
+                elderly+=1
+    print(f"children: {num_c_sur}/{children}, adults: {num_a_sur}/{adults}, elderly: {num_e_sur}/{elderly}")
+
 def run():
     load_data("titanic.csv")
     print(f"Succesfully loaded {len(records)}\n")
@@ -67,6 +95,8 @@ def run():
         display_passengers_per_gender()
     elif selected_option==4:
         display_passengers_per_age_group()
+    elif selected_option==5:
+        display_survivors_per_age_group()
     else:
         print("Error! Option not recognized")
 
