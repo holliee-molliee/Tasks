a728b1bc969d97238b69022d5075899d36fc4dbf
holliee-molliee <6jakua46@solent.ac.uk>
2022-11-18 10:39:53 +0000

    Create squares.py
    Added code to display multiple plots.

---
+++ visual/plots/squares.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+def small():
+    x=[3,4,4,3,3]
+    y=[3,3,4,4,3]
+    plt.plot(x,y, "ro:")
+
+def medium():
+    x=[2,5,5,2,2]
+    y=[2,2,5,5,2]
+    plt.plot(x,y,"gs--")
+
+def large():
+    x=[1,6,6,1,1]
+    y=[1,1,6,6,1]
+    plt.plot(x,y,"p-")
+
+small()
+medium()
+large()
+plt.show()
